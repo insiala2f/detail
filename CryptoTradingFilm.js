0eab231d867d6ac1a577d52560c804ac36b40294
insiala2f <84434303+insiala2f@users.noreply.github.com>
2022-06-29 09:15:26 -0700

    Update CryptoTradingFilm.js

--- CryptoTradingFilm.js
+++ CryptoTradingFilm.js
@@ -1,10 +1,4 @@
 var dataUrl = [
-'//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c',
-'//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
-'//anomalousporch.com/e5ntk4vw2?key=afc46c163b5e7aa15937edf1b69b82cc',
-'//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c',
-'//anomalousporch.com/fweeqvjv8d?key=d248834943f3e7e44e8f940e6f85f7e2',
-'//anomalousporch.com/bcf9j7den?key=6f0346ec565f3485f607d1f027ca2032',
 '//cryptominingdoge.blogspot.com/',
 '//cryptominingfilm.blogspot.com/',
 '//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
