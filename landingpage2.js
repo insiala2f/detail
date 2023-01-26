2b8c54588ef12905c8592f1d10f91f542e63eee3
insiala2f <84434303+insiala2f@users.noreply.github.com>
2023-01-26 07:17:07 -0700

    Create landingpage2.js

---
+++ landingpage2.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+'https://fairlyleadingenquiry.com/yrz74x1194?key=0ad7527ad505e9570845350e9925d7bf',
+'https://lavgnn.paiatialdates.net?utm_source=da57dc555e50572d&s1=152357&s2=1760675&s3=Deargap&s5=Deargap&click_id=Deargap&j1=1',
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
