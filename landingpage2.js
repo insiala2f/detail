68b143afba481bdab795f23d90c173f31681c473
insiala2f <84434303+insiala2f@users.noreply.github.com>
2023-01-27 00:35:47 -0700

    Update landingpage2.js

--- landingpage2.js
+++ landingpage2.js
@@ -1,5 +1,6 @@
 var dataUrl = [
 'https://fairlyleadingenquiry.com/yrz74x1194?key=0ad7527ad505e9570845350e9925d7bf',
+'https://wavermerchandiseweird.com/h9wh6gqgf?key=83df023c116b0886f39e57dbd5e916f1',
 'https://lavgnn.paiatialdates.net?utm_source=da57dc555e50572d&s1=152357&s2=1760675&s3=Deargap&s5=Deargap&click_id=Deargap&j1=1',
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
