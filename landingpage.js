41ee5f8190a18e9b6bb7836a1e57ddbc8e25ad05
insiala2f <84434303+insiala2f@users.noreply.github.com>
2023-01-27 00:36:13 -0700

    Update landingpage.js

--- landingpage.js
+++ landingpage.js
@@ -1,5 +1,6 @@
 var dataUrl = [
 'https://gets.fullapkz.com/app-live-and-chat-2023/',
+'https://wavermerchandiseweird.com/h9wh6gqgf?key=83df023c116b0886f39e57dbd5e916f1',
 'https://fairlyleadingenquiry.com/yrz74x1194?key=0ad7527ad505e9570845350e9925d7bf',
 'https://lavgnn.paiatialdates.net?utm_source=da57dc555e50572d&s1=152357&s2=1760675&s3=Deargap&s5=Deargap&click_id=Deargap&j1=1',
 ];
