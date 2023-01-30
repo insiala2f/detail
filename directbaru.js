7ca575e950ac9670e6b0c3c4adec1e3568d8f9a2
insiala2f <84434303+insiala2f@users.noreply.github.com>
2023-01-30 05:12:16 -0700

    Create directbaru.js

---
+++ directbaru.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+"https://bunnycam0.blogspot.com/",
+"https://snapsweet.blogspot.com/",
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
